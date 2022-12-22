dc10e967d4d13da76da0a080a89e960f5952a28d
shiniyi2 <yahyafatai@gmail.com>
2022-12-22 00:29:55 -0800

    data

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if not my_list:
-        return 0
+def best_score(a_dictionary):
+    if a_dictionary:
+        scores = list(a_dictionary.values())
+        maxScore = max(scores)
 
-    num = 0
-    den = 0
-
-    for tup in my_list:
-        num += tup[0] * tup[1]
-        den += tup[1]
-
-    return (num / den)
+        for k, v in a_dictionary.items():
+            if (a_dictionary[k] == maxScore):
+                return k
+    else:
+        return None
